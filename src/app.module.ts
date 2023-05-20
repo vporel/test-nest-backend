dc645c39f7b59637017fad5cd3d59c78d8c9abbe
unknown <dev.vporel@gmail.com>
2023-05-20 15:01:26 +0200

    changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ArticlesModule } from './articles/articles.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [ArticlesModule, DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService]
+  controllers: [AppController]
 })
 export class AppModule {}
